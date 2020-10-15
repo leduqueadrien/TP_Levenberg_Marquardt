7ff16e84194e872bb8302c3b5efcdf790c0ed4ab
adrien <aleduque@ensc.fr>
2020-10-15 13:24:53 +0200

    enlever calcule inutil

--- LevenMarqu_Generique.m
+++ LevenMarqu_Generique.m
@@ -8,34 +8,37 @@ function [a, iter, erreur] = LevenMarqu_Generique(X,Y,modele,M,epsilone,iterMax)
 
 
     plot(X,Y, '*')
-    p = length(X);
-    n = 3*M;
 
-    a = (1:3*M)';
-
+    a = ones(3*M, 1);
+    %a = (1:3*M)';
+    n = length(a);
     lambda = 0.001;
     I = eye(n);
     iter = 0;
 
     [F, gradF] = feval(modele, a, X);
-    Fa = F-Y; e = 1/2*norm(Fa)^2; Ge = gradF'*Fa; He = gradF'*gradF;
+    Fa = F-Y;
+    e = 1/2*norm(Fa)^2;
+    Ge = gradF'*Fa;
+    He = gradF'*gradF;
     while norm(Ge) >= epsilone && iter < iterMax
         d = (He+lambda*I)\(-Ge);
         [F, gradF] = feval(modele, a+d, X);
-        Fa = F-Y; e2 = 1/2*norm(Fa)^2; Ge2 = gradF'*Fa; He2 = gradF'*gradF;
+        Fa = F-Y; e2 = 1/2*norm(Fa)^2;
 
         if e2 < e
             lambda = lambda/10;
             a = a + d;
-            e = e2; Ge = Ge2; He = He2;
+            e = e2; Ge = gradF'*Fa; He = gradF'*gradF;
         else
             lambda = 10 * lambda;
         end
         
-    	iter = iter + 1;
+        iter = iter + 1;
     end
     erreur = e;
     F =  feval(modele, a, X);
     hold on
     plot(X,F)
+    Ge
 end
